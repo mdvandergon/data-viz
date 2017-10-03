b34f1102596416b0e177b7ca07cb3add4489d29f
Mark Vandergon <mdvandergon@gmail.com>
2017-10-03 17:26:40 -0500

    step 2

--- hw1/hw1.R
+++ hw1/hw1.R
@@ -8,8 +8,27 @@ library(ggplot2)
 
 # set the current directory from source file
 # cd = getwd()
-# newdir = paste0(cd, "/hw1/data")
+# newdir = paste0(cd, "/hw1")
 # setwd(newdir)
 
 acc2014 = read_sas('accident.sas7bdat')
 acc2015 = read_csv('accident.csv')
+ls()
+
+acc2014 <- acc2014 %>%
+  mutate(TWAY_ID2 = na_if(TWAY_ID2,""))
+
+table(is.na(acc2014$TWAY_ID2))
+
+setdiff(colnames(acc2015), colnames(acc2014))
+# "RUR_URB"  "FUNC_SYS" "RD_OWNER" are not in 2014
+setdiff(colnames(acc2014), colnames(acc2015))
+# "ROAD_FNC" is not in 2014
+
+acc <- bind_rows(
+  acc2014,
+  acc2015
+)
+
+count(acc, RUR_URB)
+# there are 30000 missing because RUR_URB was introduced in 2015, not in 2014 data.
