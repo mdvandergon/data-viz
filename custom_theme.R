0909b8d0017fdc033969ddb0e7814cf7ebf9e1ba
Mark Vandergon <mdvandergon@gmail.com>
2017-10-28 10:33:42 -0500

    hw5 - init

--- custom_theme.R
+++ custom_theme.R
@@ -1,11 +1,11 @@
-# ggplot theme
-library(ggplot)
-library(showtext)
-font_add("Nexa Light", "Nexa Light.otf")
-font_add("Nexa Bold", "Nexa Bold.otf")
-
 ###################
-## theme
+## custom theme
+
+library(ggplot2)
+library(extrafont)
+
+# font is here: https://fonts.google.com/specimen/Noto+Sans?selection.family=Noto+Sans:400,400i,700
+font_import(pattern = "NotoSans", prompt = FALSE)
 
 pallete<- c("#484EF4", "#1922FF",  "#0009FF",	"#B29000", 	"#FFCD00")
 pallete2 <- c("#4870F4", "#3D5399",  "#00C2FF",	"#FF8440", 	"#CC3912")
@@ -23,41 +23,49 @@ scale_color_discrete_mark <- function(){
 
 scale_color_continuous_mark <- function(){
   structure(list(
-    scale_color_gradientn(colours = pallete)
+    scale_color_gradientn(colours = pallete2)
   ))
 }
 ##################################
 
 theme_mark <- function(base_size=12, font=NA){
   
-  txt <- element_text(size = base_size, colour = "black", family="Nexa Light")
-  bold_txt <- element_text(size = base_size+2, colour = "black", family="Nexa Bold")
+  small_txt <- element_text(size = base_size*0.8, color = "#333333", family="Noto Sans")
+  txt <- element_text(size = base_size, color = "#333333", family="Noto Sans")
+  italic_txt <- element_text(size = base_size*0.8, color = "#333333", family="NotoSans-Italic")
+  bold_txt <- element_text(size = base_size+2, color = "#333333", family="NotoSans-Bold")
   
   theme_minimal(base_size = base_size, base_family = font) +
     theme(
-      ###### clean up!
       legend.key = element_blank(), 
       strip.background = element_blank(), 
-      ########### text basics
+      ########### text
       text = txt, 
-      plot.title = bold_txt, 
+      plot.title = bold_txt,
+      plot.caption = italic_txt, 
       axis.title = txt, 
-      axis.text = txt, 
+      axis.text = small_txt, 
       legend.text = txt ) +
     
-    ############## axis lines
+    ############## axis, panels, legend, etc
     theme(
-      axis.line.y = element_line(colour = "#333333", size = 0.8, linetype = "solid"),
-      axis.line.x = element_line(colour = "#333333", size = 0.8, linetype = "solid"),
+      axis.line.y = element_line(color = "#333333", size = 0.8, linetype = "solid"),
+      axis.line.x = element_line(color = "#333333", size = 0.8, linetype = "solid"),
       
-      ### legend  top and no title!
-      legend.position = "top", 
-      legend.title = element_blank(),
+      axis.ticks.x =  element_line(color = "#333333", size = 0.2, linetype = "solid"),
+      axis.ticks.y =  element_line(color = "#333333", size = 0.2, linetype = "solid"),
+      
+      panel.grid.major = element_line(color = "#f0f0f0", size = 0.2, linetype = "solid"),
+      panel.grid.minor = element_line(color = "#f0f0f0", size = 0.05, linetype = "solid"),
+      panel.border = element_blank(),
+      
+      legend.position = "right", 
+      # legend.title = element_blank(),
       legend.key = element_rect(fill = "#f8f8f8", color = "#f8f8f8"),
       legend.background = element_rect( fill = "#f8f8f8",color = "#f8f8f8", size = 0.5, linetype = "solid"),
       
       ## background
-      plot.background = element_rect(fill = "#f0f0f0",colour = "#f0f0f0",size = 0.5)
+      plot.background = element_rect(fill = "#f8f8f8",size = 0.5)
     )
 }
 
